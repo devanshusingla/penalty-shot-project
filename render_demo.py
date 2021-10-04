6f51cf78471fafd20e77a3f8523b9c2359c6e853
akshatsh49 <akshat.iitkanpur@gmail.com>
2021-10-04 21:26:29 +0530

    Updated state transition mechanics
    Made changes in indicator variable updates and rendered a scenario where the motion of the bar under acceleration is highlighted.

--- render_demo.py
+++ render_demo.py
@@ -6,12 +6,17 @@ env = gym.make('gym_env:penalty-shot-v0')
 env.reset()
 env.render()
 time.sleep(0.5)
+
+#highlights the motion of bar under acceleration
+actions = (9*[0.8] + [0.7])*10
+
 for _ in range(100):
     time.sleep(0.05)
     env.render()
     state, reward, done, info = env.step(
         env.puck_action_space.sample(), 
-        env.bar_action_space.sample()
+        # env.bar_action_space.sample()
+        actions[_]
         ) # take a random action
     if done:
         time.sleep(0.5)
