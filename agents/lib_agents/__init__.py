08484bdfcf44d35d458aa66d96cd8199bc95af5a
devanshusingla <devanshu803.singla@gmail.com>
2021-11-11 17:48:55 +0530

    add sac

--- agents/lib_agents/__init__.py
+++ agents/lib_agents/__init__.py
@@ -2,3 +2,5 @@ from .trivial import RandomPolicy
 from .trivial import SinePolicy
 
 from .value_based import DQN
+
+from.policy_based import SAC
--- example.py
+++ example.py
@@ -3,39 +3,36 @@ from torch import nn
 
 import tianshou as ts
 from tianshou.utils import TensorboardLogger, WandbLogger
+from torch.utils.tensorboard import SummaryWriter, writer
 
 from agents import TwoAgentPolicy
 from agents.lib_agents import SinePolicy
-from agents.lib_agents import DQN
+from agents.lib_agents import SAC
 
 from utils import make_render_env, make_env, make_discrete_env, make_render_discrete_env
 
-# create environment
-
-env = make_discrete_env()
-train_envs = ts.env.DummyVectorEnv([make_discrete_env for _ in range(3)])
-test_envs = ts.env.DummyVectorEnv([make_render_discrete_env for _ in range(5)])
+env = make_env()
+train_envs = ts.env.DummyVectorEnv([make_env for _ in range(10)])
+test_envs = ts.env.DummyVectorEnv([make_render_env for _ in range(5)])
 
 # creating policies
 
 p1 = SinePolicy()
-p2 = DQN(env.observation_space.shape, env.action_space['bar'].shape)(discount_factor=0.9, estimation_step=3, target_update_freq=320)
+p2 = SAC(env.action_space['bar'], env.observation_space.shape, env.action_space['bar'].shape)(n_step=1)
 policy = TwoAgentPolicy(observation_space=env.observation_space, action_space=env.action_space, policies=(p1, p2))
 
 # setup collector
 
-train_collector = ts.data.Collector(policy, train_envs, ts.data.VectorReplayBuffer(2000, 10), exploration_noise=True)
+train_collector = ts.data.Collector(policy, train_envs, ts.data.VectorReplayBuffer(2000, len(train_envs)), exploration_noise=True)
 test_collector = ts.data.Collector(policy, test_envs, exploration_noise=True)
 
 # logging
-# First sign in on wandb
-
 logger = WandbLogger(
     save_interval=1,
-    project="Penalty-Shot-Game",
+    project="test-project",
     name='Devanshu Singla',
-    entity='dsingla',
-    run_id='your-api-key-available-on-website'
+    entity='penalty-shot-project',
+    run_id='sac'
 )
 
 # training
@@ -43,5 +40,7 @@ logger = WandbLogger(
 result = ts.trainer.offpolicy_trainer(
     policy, train_collector, test_collector,
     max_epoch=10, step_per_epoch=10000, step_per_collect=10,
-    update_per_step=0.1, episode_per_test=100, batch_size=64)#, logger=logger)
+    update_per_step=0.1, episode_per_test=100, batch_size=4)#, logger=logger)
 print(f'Finished training! Use {result}')
+
+
